f1ab460b1e1e0f5623fd435979de6088bf65b53e
Connor <cjflowers@pm.me>
2025-06-20 14:21:45 +1000

    refactor endpoint naming

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -4,13 +4,13 @@ import UserService from "~/modules/user/service";
 export const GET: APIRoute = async ({ locals, redirect }) => {
   const { userId: clerkId } = locals.auth();
   if (!clerkId) {
-    console.log("Clerk userId not available after signup");
+    console.error("User does not exist after sign in");
     return new Response("Unauthorized", { status: 401 });
   }
 
   const { user, created } = await UserService.ensureExists(clerkId);
   console.log(
-    `User ${created ? "created" : "exists"} with clerkId: ${user.id}`,
+    `User ${created ? "created" : "exists"} with clerkId: ${user.clerkId}`,
   );
 
   return redirect("/");
