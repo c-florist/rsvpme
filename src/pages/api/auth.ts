764f80d5af7fb63775013bced0f743bf301075df
Connor Flowers <cjflowers@pm.me>
2025-06-20 16:10:18 +1000

    fix: clean up auth flow (#10)

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -8,10 +8,15 @@ export const GET: APIRoute = async ({ locals, redirect }) => {
     return new Response("Unauthorized", { status: 401 });
   }
 
-  const { user, created } = await UserService.ensureExists(clerkId);
-  console.log(
-    `User ${created ? "created" : "exists"} with clerkId: ${user.clerkId}`,
-  );
+  try {
+    const { user, created } = await UserService.ensureExists(clerkId);
+    console.log(
+      `User ${created ? "created" : "exists"} with clerkId: ${user.clerkId}`,
+    );
+  } catch (error) {
+    console.error("Error ensuring user exists", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 
   return redirect("/");
 };
