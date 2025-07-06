d91a17c23462513f1a6e392847c60096c711ebd0
Connor <cjflowers@pm.me>
2025-07-06 12:26:12 +1000

    refactor icons into components

---
+++ src/client/components/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as CheckIcon } from "./CheckIcon.astro";
+export { default as PlusIcon } from "./PlusIcon.astro";
+export { default as HomeIcon } from "./HomeIcon.astro";
+export { default as ArrowLeftIcon } from "./ArrowLeftIcon.astro";
+export { default as InfoIcon } from "./InfoIcon.astro";
+export { default as RefreshIcon } from "./RefreshIcon.astro";
